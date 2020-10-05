1f145904e6c839d074ebf3695ad7f6953ac98e70
Abigail Herron <abigailherron@hotmail.com>
2020-10-05 12:04:31 +0100

    Update Program.cs

--- Labsheet1/Exercise5/Program.cs
+++ Labsheet1/Exercise5/Program.cs
@@ -2,11 +2,12 @@
  Name: Abigail Herron
  ID: S00200536
  Year: 2
+ Date: 05/10/2020
  Link to GitHub for Lab1: https://github.com/AbigailHerron/ObjectOrientatedProgramming/tree/main/Labsheet1
 
  Description: 1) Revision of If statements
               2) Calculates user's grade based on their grade percentage
- Parameters: User grade
+ Parameters: User grade, Subject difficulty
  #########################################################################################################*/
 using System;
 
