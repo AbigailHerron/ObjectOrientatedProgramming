edd86a0704bd51472d2c3f05e1e9d2ec0e889d0b
Abigail Herron <abigailherron@hotmail.com>
2020-10-05 10:11:51 +0100

    Create Program.cs

---
+++ Labsheet1/Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*#################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+
+ Description: Revision of If statements
+ Parameters: User grade
+ #################################################################*/
+using System;
+
+namespace Labsheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring Variables
+
+
+
+        }// End Main
+    }// End Class
+}// End Namespace
