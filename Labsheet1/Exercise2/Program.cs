e5ce6fa5f935dd8f0d36f66182ed18ae76903a8e
Abigail Herron <abigailherron@hotmail.com>
2020-10-05 10:40:04 +0100

    Update Program.cs

--- Labsheet1/Exercise2/Program.cs
+++ Labsheet1/Exercise2/Program.cs
@@ -5,7 +5,7 @@
   Link to GitHub for Lab1: https://github.com/AbigailHerron/ObjectOrientatedProgramming/tree/main/Labsheet1
 
  Description: 1) Revision of Switch statements
-              2) Calculates user's points based on the inputted percentage grade
+              2) Calculates user's points based on the inputed type of grade
  Parameters: User grade
  ##########################################################################################################*/
 using System;
@@ -17,19 +17,43 @@ namespace Labsheet1
         static void Main(string[] args)
         {
             // Declaring Variables
-            int userGrade = 0, userPoints = 0;
-            int[] gradeBrackets = new int[] {90, 80, 70, 60, 50, 40};
+            string userGrade;
+            int userPoints;
 
             // Getting Data from Users
-            Console.Write("Enter percentage grade :  ");
-            userGrade = int.Parse(Console.ReadLine());
+            Console.Write("Enter type of grade :  ");
+            userGrade = Console.ReadLine().ToUpper();
 
             // Calculating Points
             switch(userGrade)
             {
-                
+                case "H1":
+                    userPoints = 100;
+                    break;
+                case "H2":
+                    userPoints = 88;
+                    break;
+                case "H3":
+                    userPoints = 77;
+                    break;
+                case "H4":
+                    userPoints = 66;
+                    break;
+                case "H5":
+                    userPoints = 56;
+                    break;
+                case "H6":
+                    userPoints = 46;
+                    break;
+                case "H7":
+                    userPoints = 37;
+                    break;
+                default:
+                    userPoints = 0;
+                    break;
+            }// end of switch block
 
-            }
+            Console.WriteLine($"\nPoints awarded : {userPoints}");
 
 
         }// End Main
