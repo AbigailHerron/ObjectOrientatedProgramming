8c1cc8e90070327fd71d2bd023fb112ae0fa6c43
Abigail Herron <abigailherron@hotmail.com>
2020-10-12 10:46:01 +0100

    Update Program.cs

--- Labsheet3/Exercise3/Program.cs
+++ Labsheet3/Exercise3/Program.cs
@@ -4,8 +4,8 @@
  Year: 2
  Date: 12/10/2020
 
- Description:  Returns the cost of a product based on the entered description
- Parameters: Item
+ Description:  Revision of Arrays and Methods
+ Parameters: randArray
  #################################################################################################*/
 using System;
 
@@ -15,7 +15,41 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
-            
+            //Declaring variables
+            int[] randArray = new int[4] {35, 22, 89, 9000};
+
+            // Testing values in randArray
+            foreach(int num in randArray)
+            {
+                Console.Write(" " + num);
+            }
+
+            // Changing value of randArray elements
+            Zero(randArray);
+
+
+            // Printing new results to screen
+            Console.WriteLine();
+            foreach(int num in randArray)
+            {
+                Console.Write(" " + num);
+            }
         }// end Main()
+
+
+        /*---------
+         Method: Zero()
+                1) Takes an int array
+                2) Assigns each element of the array as 0
+        ---------*/
+        static private void Zero(int[] arry)
+        {
+
+            for(int i = 0; i < arry.Length; i++)
+            {
+                arry[i] = 0;
+            }
+        }// end Zero()
+
     }// end Class
 }// end Namespace
