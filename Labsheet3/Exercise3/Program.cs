f3c7759c9d5892172917cab1718a19943eafa1b7
Abigail Herron <abigailherron@hotmail.com>
2020-10-12 10:29:42 +0100

    Create Program.cs

---
+++ Labsheet3/Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*#################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 12/10/2020
+
+ Description:  Returns the cost of a product based on the entered description
+ Parameters: Item
+ #################################################################################################*/
+using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
