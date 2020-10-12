3176733c180d7a17853b131fee8e4d4800392c45
Abigail Herron <abigailherron@hotmail.com>
2020-10-12 11:44:05 +0100

    Create Program.cs

---
+++ Labsheet3/Exercise7/Program.cs
@@ -0,0 +1,22 @@
+﻿/*#################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 12/10/2020
+
+ Description:  Displays the average score rating for three different movies
+               - Main focus is 2D Arrays
+ Parameters: MovieTitle, AverageScore, Scores
+ #################################################################################################*/
+using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
