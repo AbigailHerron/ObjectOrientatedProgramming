405401aa9a5680cbee263d93eab02fe78373a8b4
Abigail Herron <abigailherron@hotmail.com>
2020-10-12 11:16:12 +0100

    Create Program.cs

---
+++ Labsheet3/Exercise5/Program.cs
@@ -0,0 +1,21 @@
+﻿/*#################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 12/10/2020
+
+ Description:  Revision of Methods
+ Parameters: Num1, Num2
+ #################################################################################################*/
+using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
