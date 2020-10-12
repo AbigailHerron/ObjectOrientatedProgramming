ca8cde683b2b8632f5a50e3d33d263c504c93321
Abigail Herron <abigailherron@hotmail.com>
2020-10-12 10:11:53 +0100

    Create Program.cs

---
+++ Labsheet3/Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*#################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 12/10/2020
+
+ Description:  Tests whether a user can enter a site or not based on their age
+ Parameters: userAge
+ #################################################################################################*/
+using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
