b11a9404dc7d395f46f26332be88cf8dccb93f6f
Abigail Herron <abigailherron@hotmail.com>
2020-10-12 03:09:12 +0100

    Create Program.cs

---
+++ Labsheet2/Exercise3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*#################################################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 12/10/2020
+
+ Description: Reading student grades from text file and prints the results to the screen
+              - Revision of arrays
+
+ Paramenters: Grades, Points
+ #################################################################################################################################*/
+using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }// end Main()
+    }// end Class
+}// end Namespace
