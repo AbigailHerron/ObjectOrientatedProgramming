52fabc73e06be76eb5f73706ba9673dc1c186afa
Abigail Herron <abigailherron@hotmail.com>
2020-10-13 12:01:30 +0100

    Update Program.cs

--- Labsheet3/Exercise7/Program.cs
+++ Labsheet3/Exercise7/Program.cs
@@ -9,14 +9,46 @@
  Parameters: MovieTitle, AverageScore, Scores
  #################################################################################################*/
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Exercise7
+namespace Exercise6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaring variables
+            string[] movieTitles = new string[3] { "Avengers Endgame", "Spider-Man: Far From Home", "Once Upon A Time in Hollywood" };
+            int[] averageScores = new int[3];
+            int[][] scores = new int[][]
+                                {
+                                new int[] {3,4}, // [0,x]
+                                new int[] {1,2,1,3},// [1,x]
+                                new int[] {5,4,2}// [2,x]
+                                };
+            int score = 0;
+
+            // Calculating averages
+            for (int i = 0; i < scores.GetLength(0); i++)
+            {
+                for (int j = 0; j < scores[i].Length; j++)
+                {
+                    
+                    score += scores[i][j]; 
+                }
+                averageScores[i] = score / scores[i].Length;
+                score = 0;
+            }// end nested for loop
+
+            // Printing results to screen
+            for (int i = 0; i < movieTitles.Length; i++)
+            {
+                Console.WriteLine("The average score for '{0}' is {1:f}", movieTitles[i], averageScores[i]);
+            }// end for block
+
         }// end Main()
     }// end Class
 }// end Namespace
