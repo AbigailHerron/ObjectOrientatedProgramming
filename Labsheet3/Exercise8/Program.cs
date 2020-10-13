083471ba8620e870cb6c46ba7b7b51a232d73771
Abigail Herron <abigailherron@hotmail.com>
2020-10-13 12:24:16 +0100

    Update Program.cs

--- Labsheet3/Exercise8/Program.cs
+++ Labsheet3/Exercise8/Program.cs
@@ -9,6 +9,7 @@
  Parameters: PlayerName
  #################################################################################################*/
 using System;
+using System.Collections.Generic;
 
 namespace Exercise8
 {
@@ -16,7 +17,38 @@ namespace Exercise8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaring variables
+            List<string> teamNames = new List<string>();
+            const string QUIT = "-1";
+            string userInput = "?";
+            int counter = 1;
+
+            // Getting team names from user
+            Console.WriteLine("Enter '-1' when all team players have been entered");
+            do
+            {
+                Console.WriteLine($"Enter name of team member {counter}:  ");
+                userInput = Console.ReadLine();
+                Console.WriteLine(); // new line for neatness
+
+                if (userInput != QUIT)
+                    teamNames.Add(userInput);
+
+                counter++;
+            } while (userInput != QUIT);
+
+
+            // Organising teamNames in alphabetical order
+            teamNames.Sort();
+
+            // Displaying team names
+            Console.WriteLine("Full Team Roster:");
+            Console.WriteLine("-------------------------------------");
+            foreach(string name in teamNames)
+            {
+                Console.WriteLine(name);
+            }// end foreach block
+
         }// end Main()
     }// end Class
 }// end Namespace
