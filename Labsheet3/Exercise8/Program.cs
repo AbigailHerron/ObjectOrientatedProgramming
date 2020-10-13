3590b29aabf9fa225d8654aa99d7609d5d149dcc
Abigail Herron <abigailherron@hotmail.com>
2020-10-13 12:01:38 +0100

    Create Program.cs

---
+++ Labsheet3/Exercise8/Program.cs
@@ -0,0 +1,22 @@
+﻿/*#################################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 12/10/2020
+
+ Description:  Records and displays the name of each player on a team
+               - Main focus is Lists
+ Parameters: PlayerName
+ #################################################################################################*/
+using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }// end Main()
+    }// end Class
+}// end Namespace
