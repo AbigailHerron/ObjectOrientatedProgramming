8398c4562a82e39a530132daa28c94285bffa9e8
Abigail Herron <abigailherron@hotmail.com>
2020-10-19 12:18:11 +0100

    Create CashRegister.cs

---
+++ Exercise1/TestCashRegister/CashRegister.cs
@@ -0,0 +1,29 @@
+﻿/*#############################################################################################
+ Name: Abigail Herron
+ ID: s00200536
+ Date: 19/10/20
+
+ Description: Basic cash register layout
+ Properties: Tally, NumberOfItems
+ Methods: AddItem(), ToString() (replaces normal ToString())
+ #############################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCashRegister
+{
+    class CashRegister
+    {
+        /* Declaring Properties ----------------------------------------------*/
+
+        //Read Only
+        private double total { get; }
+        
+            
+        // Editable
+        public double Tally { get; set; }
+        public int NumberOfItems { get; set; }
+
+    }// end CashRegister
+}// end TestCashRegister
