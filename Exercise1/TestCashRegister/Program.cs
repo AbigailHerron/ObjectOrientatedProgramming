64efaaf2ba89de4da751d379cb5de1e3038bd17b
Abigail Herron <abigailherron@hotmail.com>
2020-10-19 12:18:15 +0100

    Create Program.cs

---
+++ Exercise1/TestCashRegister/Program.cs
@@ -0,0 +1,22 @@
+﻿/*#############################################################################################
+ Name: Abigail Herron
+ ID: s00200536
+ Date: 19/10/20
+
+ Description:  1) Creates and displays the details of several CashRegister objects
+               2) Making use of the methods defined in CashRegister class to tally
+                  up a total for a sale
+ Paramenters: Reg1, Reg2, Reg3, Reg4
+ #############################################################################################*/
+using System;
+
+namespace TestCashRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }// end Main()
+    }// end Class
+}// end TestCashRegister
