03b2060ef71cf877b77af08159c4b239cded2fae
Abigail Herron <abigailherron@hotmail.com>
2020-10-19 10:43:25 +0100

    Update Car.cs

--- Exercise1/Labsheet4/Car.cs
+++ Exercise1/Labsheet4/Car.cs
@@ -5,6 +5,7 @@
 
  Description: Basic car layout
  Properties: Make, Model, Current_Speed and Engine_Size
+ Methods: DisplayCarInfo(), ToString() (overrides original ToString()), Accelerate()
  #############################################################################################*/
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,8 @@ namespace Labsheet4
 {
     class Car
     {
-        // make property
+        /* Declaring Properties ----------------------------------------------*/
+            // make property
         private string make;
         public string Make
         {
@@ -22,9 +24,7 @@ namespace Labsheet4
             set { this.make = value; }
         }
 
-
-
-        // model property
+            // model property
         private string model;
         public string Model
         {
@@ -32,7 +32,7 @@ namespace Labsheet4
             set { this.model = value; }
         }
 
-        // current speed property
+            // current speed property
         private int currentSpeed;
         public int CurrentSpeed
         {
@@ -41,13 +41,57 @@ namespace Labsheet4
         }
 
 
-        // engin size property
+            // engin size property
         private double engineSize;
         public double EngineSize
         {
             get { return this.engineSize; }
             set { this.engineSize = value; }
         }
+
+        /* Declaring Methods -------------------------------------------------*/
+
+        /*---------
+        Method: DisplayCarInfo()
+                1) Prints the Make, Model, CurrentSpeed and EngineSize 
+                   properties to the screen on different lines
+        ---------*/
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Car Make: {Make}");
+            Console.WriteLine($"Car Model: {Model}");
+            Console.WriteLine($"Current Speed: {CurrentSpeed}");
+            Console.WriteLine($"Engine Size: {EngineSize}");
+        }// end DisplayCarInfo()
+
+
+
+
+        /*---------
+        Method: ToString()
+                1) Overrides the ToString() method
+                2) Prints the Make, Model, CurrentSpeed and EngineSize 
+                   properties to the screen on different lines
+        ---------*/
+        public override string ToString()
+        {
+            return string.Format($"Car Make: {Make}" +
+                                 $"\nCar Model: {Model}" +
+                                 $"\nCurrent Speed: {CurrentSpeed}" +
+                                 $"\nEngine Size: {EngineSize}");
+        }// end ToString()
+
+
+        /*---------
+        Method: Accelerate()
+                1) Adds 10 to the CurrentSpeed property of the object
+                2) Prints the CurrentSpeed of the object to the screen
+        ---------*/
+        public void Accelerate()
+        {
+            CurrentSpeed += 10;
+            Console.WriteLine($"Current Speed is {CurrentSpeed}");
+        }// end Accelerate()
 
     }// end Car
 }// end Namespace
