22249b5a47edf14e0cb3c1f86193e5a910171187
Abigail Herron <abigailherron@hotmail.com>
2020-11-16 02:13:28 +0000

    Update Program.cs

--- Labsheet5/Labsheet5/Program.cs
+++ Labsheet5/Labsheet5/Program.cs
@@ -15,21 +15,23 @@ namespace Labsheet5
     {
         static void Main(string[] args)
         {
-            Game g1 = new Game("Monopoly", 19.99m);
-            Game g2 = new Game() { ReleaseDate = new DateTime(2000, 6, 15) };
+            /* These no longer work when Game class was declared abstract: as in, you can't make an instance
+               of an abstract class */
+            //Game g1 = new Game("Monopoly", 19.99m);
+            //Game g2 = new Game() { ReleaseDate = new DateTime(2000, 6, 15) };
 
-            Game g3 = new Game("Clue", 15.50m);
-            Game g4 = new Game();
+            //Game g3 = new Game("Clue", 15.50m);
+            //Game g4 = new Game();
 
-            DateTime date1 = new DateTime(1995,13,09);
-            //ComputerGame cg1 = new ComputerGame("Crash Bandicoot", 25.99m, new DateTime(1996, 9, 9), "12");
-            //ComputerGame cg2 = new ComputerGame("Space Invaders", 5.99m, new DateTime(1978, 3, 10), "3");
+            ComputerGame cg1 = new ComputerGame("Crash Bandicoot", 25.99m, new DateTime(1996, 9, 9), "12");
+            ComputerGame cg2 = new ComputerGame("Space Invaders", 5.99m, new DateTime(1978, 3, 10), "3");
 
+            // These no longer work now that Game class is abstract
+            //DisplayGame(g3);
+            //DisplayGame(g4);
 
-            DisplayGame(g3);
-            DisplayGame(g4);
-            //DisplayGame(cg1);
-           // DisplayGame(cg2);
+            DisplayGame(cg1);
+            DisplayGame(cg2);
 
         }// end Main()
 
