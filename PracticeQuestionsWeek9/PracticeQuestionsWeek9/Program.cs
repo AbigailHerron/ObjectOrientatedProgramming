f9be781bc59af2310215b9fb8baec510605d6230
Abigail Herron <abigailherron@hotmail.com>
2020-11-23 10:19:29 +0000

    Create Program.cs

---
+++ PracticeQuestionsWeek9/PracticeQuestionsWeek9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeQuestionsWeek9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
