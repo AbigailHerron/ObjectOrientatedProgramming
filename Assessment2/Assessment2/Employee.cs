8293e9af6eddeb9956256bab74fa882515ee1411
AbigailHerron <abigailherron@hotmail.com>
2020-12-15 12:38:46 +0000

    Update Employee.cs

--- Assessment2/Assessment2/Employee.cs
+++ Assessment2/Assessment2/Employee.cs
@@ -1,15 +1,16 @@
-﻿/*########################################################################################################
+﻿/*###########################################################################################################################
  Name: Abigail Herron
  ID: S00200536
  Year: 2
  Date: 15/12/20
+ GitHub Link: https://github.com/AbigailHerron/ObjectOrientatedProgramming/blob/main/Assessment2/Assessment2/Employee.cs
 
  Description: 1) A blueprint for an Employee object
               2) Is intended to be an abstract base class
  Properties: FirstName, LastName
  Methods: CalculateMonthlyPay
  Constructors: None
- #######################################################################################################*/
+ ##########################################################################################################################*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
