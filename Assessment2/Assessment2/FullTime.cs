9131ccd3199fed94e802e9c4e1e18c74426b7b15
AbigailHerron <abigailherron@hotmail.com>
2020-12-15 02:31:30 +0000

    Update FullTime.cs

--- Assessment2/Assessment2/FullTime.cs
+++ Assessment2/Assessment2/FullTime.cs
@@ -1,15 +1,15 @@
-﻿/*######################################################################################
+﻿/*#########################################################################################################
  Name: Abigail Herron
  ID: S00200536
  Year: 2
  Date: 15/12/20
 
  Description: 1) A blueprint for a FullTime employee object
-              2) Is a subclass of the Employee class
- Added Properties: Salary
- Methods: 
+              2) Is a derived class of the Employee class
+ Added Properties: none
+ Changed Methods: CalculatePay
  Constructors: Default, All
- ######################################################################################*/
+ ########################################################################################################*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +20,17 @@ namespace Assessment2
 {
     class FullTime : Employee
     {
-        /*PROPERTIES ------------------------------------------------------------------*/
-        public decimal Salary { get; set; }
-
-        /*CONSTRUCTORS ----------------------------------------------------------------*/
-
+        /*METHODS --------------------------------------------------------------------------------------*/
+        /*Method: CalculatePay() (override)
+                  1) Overrides base class CalculatePay()
+                  2) Takes in a salary (annual ammount) as a decimal and divides it by 12 to get
+                     the monthly pay 
+                  3) Passes this value back to the original CalculatePay() method in the base class */
+        protected override void CalculatePay(decimal ammount)
+        {
+            decimal monthly = ammount / 12;
+            base.CalculatePay(monthly);
+        }// end CalculatePay()
 
 
     }// end FullTime class
