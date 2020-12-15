bf7e54f44abd508d41acd83ad3a60ea5930a0d69
AbigailHerron <abigailherron@hotmail.com>
2020-12-15 12:34:17 +0000

    Update PartTimeEmployee.cs

--- Assessment2/Assessment2/PartTimeEmployee.cs
+++ Assessment2/Assessment2/PartTimeEmployee.cs
@@ -7,7 +7,7 @@
  Description: 1) A blueprint for a PartTimeEmployee object
               2) Is derived from the abstract Employee class
  Added Properties: HourlyRate, HoursWorked
- Altered Methods: CalculateMonthlyPay
+ Altered Methods: CalculateMonthlyPay, ToString
  Constructors: Default, FirstName, FirstName and LastName, FirstName LastName and HourlyRate, All
  #######################################################################################################*/
 using System;
@@ -38,8 +38,18 @@ namespace Assessment2
         }// end CalculateMonthlyPay()
 
 
+        /*Method: ToString() (override) 
+                  1) Overrides original ToString() method
+                  2) Returns a string value format of LastName (in uppercase), FirstName and type of
+                     employee */
+        public override string ToString()
+        {
+            return string.Format($"{this.FirstName.ToUpper()}, {this.LastName} - Part Time");
+        }// end ToString()
+
 
         /*CONSTRUCTORS ---------------------------------------------------------------------------------*/
+        #region Constructor Chain
         /*Constructor: Default
                        1) Declares an object with a default FirstName, LastName, HourlyRate and
                           HoursWorked properties
@@ -79,7 +89,7 @@ namespace Assessment2
         }// end FirstName, LastName and HourlyRate constructor
 
 
-
+        #endregion
         /*Constructor: All
                        1) Takes in two string values for FirstName and LastName properties 
                        2) Takes in a decimal value for the Salary property 
