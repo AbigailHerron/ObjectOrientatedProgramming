267bc07f26306643ea01ac5c9633adf6e714221e
AbigailHerron <abigailherron@hotmail.com>
2020-12-15 01:34:52 +0000

    Create FullTime.cs

---
+++ Assessment2/Assessment2/FullTime.cs
@@ -0,0 +1,31 @@
+﻿/*######################################################################################
+ Name: Abigail Herron
+ ID: S00200536
+ Year: 2
+ Date: 15/12/20
+
+ Description: 1) A blueprint for a FullTime employee object
+              2) Is a subclass of the Employee class
+ Added Properties: Salary
+ Methods: 
+ Constructors: Default, All
+ ######################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    class FullTime : Employee
+    {
+        /*PROPERTIES ------------------------------------------------------------------*/
+        public decimal Salary { get; set; }
+
+        /*CONSTRUCTORS ----------------------------------------------------------------*/
+
+
+
+    }// end FullTime class
+}// end Namespace
